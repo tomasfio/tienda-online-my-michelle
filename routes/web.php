564d3d4826b1e468d3ea580873e925c9be73f9eb
Tomas Fiorenza <tomas.fiorenza@outlook.com>
2020-08-06 19:36:47 -0300

    Se dividio el sitio en subdominios

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,23 @@
 use Illuminate\Support\Facades\Route;
 
 Auth::routes();
-Route::get('/', 'HomeController@index');
 
-Route::group(['middleware' => ['auth', 'permission:gestionar_pagina']], function () {
-    Route::get('/usuarios', 'UserController@index')->middleware('permission:users_abm');
-    Route::resource('/productos', 'ProductoController');
-    Route::resource('/subcategorias', 'SubcategoriaController');
+Route::group(['domain' => env('APP_URL')], function(){
+    Route::get('/', function(){
+        return '';
+    });
+});
 
-    Route::get('/productos/{producto}/galeria', 'GaleriaController@index')->name('product.galleria.index');
-    Route::post('/productos/{producto}/galeria', 'GaleriaController@add')->name('product.galleria.add');
-    Route::delete('/productos/{producto}/galeria', 'GaleriaController@delete')->name('product.galeria.destroy');
-    Route::patch('/productos/{producto}/galeria', 'GaleriaController@update')->name('product.galeria.update');
+Route::group(['domain' => 'gestion.localhost'], function(){
+    Route::get('/', 'HomeController@index');
+    Route::group(['middleware' => ['auth', 'permission:gestionar_pagina']], function () {
+        Route::get('/usuarios', 'UserController@index')->middleware('permission:users_abm');
+        Route::resource('/productos', 'ProductoController');
+        Route::resource('/subcategorias', 'SubcategoriaController');
+    
+        Route::get('/productos/{producto}/galeria', 'GaleriaController@index')->name('product.galleria.index');
+        Route::post('/productos/{producto}/galeria', 'GaleriaController@add')->name('product.galleria.add');
+        Route::delete('/productos/{producto}/galeria', 'GaleriaController@delete')->name('product.galeria.destroy');
+        Route::patch('/productos/{producto}/galeria', 'GaleriaController@update')->name('product.galeria.update');
+    });
 });
